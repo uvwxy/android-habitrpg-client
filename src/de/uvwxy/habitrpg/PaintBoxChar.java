f1b8b249b27d347cedeb3ae9f42d24e184167cd6
Paul Smith <code@uvwxy.de>
2013-05-05 12:35:09 +0200

    added paintboxchar, glyphicons (start, heart), added more licence info

---
+++ src/de/uvwxy/habitrpg/PaintBoxChar.java
@@ -0,0 +1,27 @@
+package de.uvwxy.habitrpg;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import de.uvwxy.paintbox.PaintBox;
+
+public class PaintBoxChar extends PaintBox {
+
+	public PaintBoxChar(Context context) {
+		super(context);
+	}
+
+	public PaintBoxChar(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public PaintBoxChar(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+
+	}
+
+}
